0cfbbfae8d63aad2e48460b5d501f4f91b8f3984
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 17:00:57 +0100

    add playAnimation method to class movableObject

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,3 +1,18 @@
 class MovableObject extends DrawableObject {
-    
+
+    speed = 0.15;
+    otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+    energy = 100;
+    lastHit = 0;
+
+    playAnimation(images) {
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
 }
+
