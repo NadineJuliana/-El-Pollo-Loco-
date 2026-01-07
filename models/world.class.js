3ed0e67c14520b225b51cf26b9ece2938a7f14f8
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 16:42:32 +0100

    add draw method and add objects to map/ add objectstoMap methods

--- models/world.class.js
+++ models/world.class.js
@@ -1,1 +1,66 @@
-class World {}
+class World {
+    character = new Character();
+    level = level1;
+    canvas;
+    ctx;
+    keyboard;
+    camera_x = 0;
+    statusBar = new StatusBar();
+    throwableObjects = [];
+
+    constructor(canvas, keyboard,) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.keyboard = keyboard;
+        this.draw();
+        this.setWorld();
+        this.run();
+    }
+
+    setWorld() {
+        this.character.world = this;
+    }
+
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.translate(this.camera_x, 0);
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
+
+        this.ctx.translate(-this.camera_x, 0);
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
+
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.enemies);
+        this.ctx.translate(-this.camera_x, 0);
+
+        let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+        });
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach((object) => {
+            this.addToMap(object);
+        });
+    }
+
+    addToMap(mO) {
+        if (mO.otherDirection) {
+            this.flipImage(mO);
+        }
+
+        mO.draw(this.ctx);
+        mO.drawFrame(this.ctx);
+
+        if (mO.otherDirection) {
+            this.flipImageBack(mO);
+        }
+
+    }
+
+   
+}
