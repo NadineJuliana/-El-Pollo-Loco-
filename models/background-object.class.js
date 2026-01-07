8246c223d79c771f435c686274ec2c6b9f77070a
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 16:46:57 +0100

    add constructor an variables to class BackgroundObjects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,3 +1,10 @@
 class BackgroundObject extends MovableObject {
+    width = 720;
+    height = 480;
+    y = 0;
     
+    constructor(imagePath, x) {
+        super().loadImage(imagePath);
+        this.x = x;
+    }
 }
