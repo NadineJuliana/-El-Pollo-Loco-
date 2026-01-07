4aadf9b0847392ff1635c67ccdc82eeb08d17f59
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 16:53:43 +0100

    add images, constructor and animate method to class endboss

--- models/enboss.class.js
+++ models/enboss.class.js
@@ -1,3 +1,27 @@
 class Endboss extends MovableObject {
-    
+    height = 400;
+    width = 250;
+    y = 50;
+
+
+    IMAGES_WALKING = [
+        'img/3_enemie_boss_chicken/1_walk/G1.png',
+        'img/3_enemie_boss_chicken/1_walk/G2.png',
+        'img/3_enemie_boss_chicken/1_walk/G3.png',
+        'img/3_enemie_boss_chicken/1_walk/G4.png',
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 2500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+
+    }
 }
