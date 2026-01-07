9662d54d6a463736b748bfdfec485bb99ef3b530
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 19:01:10 +0100

    add constructor and throw method to class throwableObject

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,3 +1,18 @@
 class ThrowableObject extends MovableObject {
-    
+    constructor(x, y) {
+        super().loadImage('img/4_salsa_bottle/salsa_bottle.png');
+        this.x = x;
+        this.y = y;
+        this.height = 70;
+        this.width = 60;
+        this.throw();
+    }
+
+    throw() {
+        this.speedY = 30;
+        this.applyGravity();
+        setInterval(() => {
+            this.x += 10;
+        }, 25);
+    }
 }
