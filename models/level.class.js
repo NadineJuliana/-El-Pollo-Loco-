ca7e19c102973aa8be7fa5760001d4c94bff0815
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 16:49:35 +0100

    add constructor and variables to class Level

--- models/level.class.js
+++ models/level.class.js
@@ -1,3 +1,12 @@
 class Level {
-    
+    enemies;
+    clouds;
+    backgroundObjects;
+    level_end_x = 2400;
+
+    constructor(enemies, clouds, backgroundObjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObjects = backgroundObjects;
+    }
 }
