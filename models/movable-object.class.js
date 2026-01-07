b270bacc3effd38b24c944432accecd24e5e27cd
NadineJuliana <nadine.juliana.b@web.de>
2026-01-07 17:04:01 +0100

    add applyGravity and isAboveGround methods to class movableObject

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -7,6 +7,25 @@ class MovableObject extends DrawableObject {
     energy = 100;
     lastHit = 0;
 
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        if (this instanceof ThrowableObject) {
+            return true;
+        } else {
+            return this.y < 160;
+        }
+
+    }
+
     isColliding(mo) {
         return this.x + this.width > mo.x &&
             this.y + this.height > mo.y &&
