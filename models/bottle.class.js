694e1530abb5e80593c83df17679a4d7dfa4b0de
NadineJuliana <nadine.juliana.b@web.de>
2026-01-09 14:47:18 +0100

    add images, constructor and setrandomImage method to class bottle

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,3 +1,23 @@
 class Bottle extends DrawableObject {
-    
+    height = 70;
+    width = 70;
+    y = 360;
+
+    bottlesOnGround = ImageHub.salsa_bottle.on_Ground;
+
+    offset = { top: 15, right: 15, bottom: 10, left: 20 };
+
+    constructor() {
+        super();
+        this.loadImages(this.bottlesOnGround);
+        this.setRandomImage();
+        this.x = 250 + Math.random() * 2500;
+    }
+
+    setRandomImage() {
+        const imagePaths = Object.keys(this.imageCache);
+        const randomIndex = Math.floor(Math.random() * imagePaths.length);
+        const randomPath = imagePaths[randomIndex];
+        this.img = this.imageCache[randomPath];
+    }
 }
