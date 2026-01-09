17e07a1db2835e1f66b7edd49b9b618631a4c21b
NadineJuliana <nadine.juliana.b@web.de>
2026-01-09 11:54:47 +0100

    integrate imageHub into class character, chicken and endboss

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,17 +1,21 @@
 class Chicken extends MovableObject {
     y = 360;
-    height = 60;
-    width = 80;
-    IMAGES_WALKING = [
-        'img/2_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        'img/2_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        'img/2_enemies_chicken/chicken_normal/1_walk/3_w.png'
-    ];
+    height = 50;
+    width = 70;
+    // IMAGES_WALKING = [
+    //     'img/2_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    //     'img/2_enemies_chicken/chicken_normal/1_walk/2_w.png',
+    //     'img/2_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    // ];
+
+    bigChickenWalking = ImageHub.chicken_normal.walk;
+    bigChickenDead = ImageHub.chicken_normal.dead;
 
     constructor() {
         super().loadImage('img/2_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.loadImages(this.IMAGES_WALKING);
-        this.x = 200 + Math.random() * 500;
+        this.loadImages(this.bigChickenWalking);
+        this.loadImages(this.bigChickenDead);
+        this.x = 800 + Math.random() * 500;
         this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
@@ -20,9 +24,9 @@ class Chicken extends MovableObject {
         setInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
-        
+
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            this.playAnimation(this.bigChickenWalking);
         }, 200);
 
     }
--- models/enboss.class.js
+++ models/enboss.class.js
@@ -1,28 +1,45 @@
 class Endboss extends MovableObject {
     height = 400;
     width = 250;
-    y = 50;
+    y = 60;
 
     offset = { top: 90, right: 40, bottom: 40, left: 40 };
 
-
-    IMAGES_WALKING = [
-        'img/3_enemie_boss_chicken/1_walk/G1.png',
-        'img/3_enemie_boss_chicken/1_walk/G2.png',
-        'img/3_enemie_boss_chicken/1_walk/G3.png',
-        'img/3_enemie_boss_chicken/1_walk/G4.png',
-    ];
+    endbossWalking = ImageHub.endboss.walk;
+    endbossAlert = ImageHub.endboss.alert;
+    endbossAttack = ImageHub.endboss.attack;
+    endbossHurt = ImageHub.endboss.hurt;
+    endbossDead = ImageHub.endboss.dead;
 
     constructor() {
-        super().loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
-        this.x = 2500;
+        super().loadImage(this.endbossWalking[0]);
+        this.loadImages(this.endbossWalking);
+        this.loadImages(this.endbossAlert);
+        this.loadImages(this.endbossAttack);
+        this.loadImages(this.endbossHurt);
+        this.loadImages(this.endbossDead);
+        this.x = 2800;
         this.animate();
     }
 
+
+    // IMAGES_WALKING = [
+    //     'img/3_enemie_boss_chicken/1_walk/G1.png',
+    //     'img/3_enemie_boss_chicken/1_walk/G2.png',
+    //     'img/3_enemie_boss_chicken/1_walk/G3.png',
+    //     'img/3_enemie_boss_chicken/1_walk/G4.png',
+    // ];
+
+    // constructor() {
+    //     super().loadImage(this.IMAGES_WALKING[0]);
+    //     this.loadImages(this.IMAGES_WALKING);
+    //     this.x = 2500;
+    //     this.animate();
+    // }
+
     animate() {
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            this.playAnimation(this.endbossWalking);
         }, 200);
 
     }
