ce139c99f35130333c9a673d9ddd3d88c5df8248
NadineJuliana <nadine.juliana.b@web.de>
2026-03-17 17:04:04 +0100

    refactor and enhance enboss methods for attack charackter

--- models/enboss.class.js
+++ models/enboss.class.js
@@ -1,242 +1,267 @@
 class Endboss extends MovableObject {
-    height = 400;
-    width = 250;
-    y = 60;
+  height = 400;
+  width = 250;
+  y = 60;
 
-    offset = { top: 90, right: 40, bottom: 40, left: 40 };
-    energy = 100;
+  offset = { top: 90, right: 40, bottom: 40, left: 40 };
+  energy = 100;
 
-    speed = 0.4;
+  speed = 0.4;
 
-    isDeadAnimationPlaying = false;
-    deadAnimationFrame = 0;
-    attackCooldown = 1000;
-    lastAttack = 0;
-    state = 'idle';
-    spawnX = 2800;
-    alertPlayed = false;
-    lastTimeSeen = 0;
-    world;
-    endbossWalking = ImageHub.endboss.walk;
-    endbossAlert = ImageHub.endboss.alert;
-    endbossAttack = ImageHub.endboss.attack;
-    endbossHurt = ImageHub.endboss.hurt;
-    endbossDead = ImageHub.endboss.dead;
+  isDeadAnimationPlaying = false;
+  deadAnimationFrame = 0;
+  attackCooldown = 1000;
+  lastAttack = 0;
+  state = "idle";
+  spawnX = 2800;
+  alertPlayed = false;
+  lastTimeSeen = 0;
+  world;
+  otherDirection = false;
+  turning = false;
+  turnStartTime = 0;
+  turnDuration = 400;
+  endbossWalking = ImageHub.endboss.walk;
+  endbossAlert = ImageHub.endboss.alert;
+  endbossAttack = ImageHub.endboss.attack;
+  endbossHurt = ImageHub.endboss.hurt;
+  endbossDead = ImageHub.endboss.dead;
 
-    constructor(world) {
-        super();
-        this.world = world;
-        this.maxEnergy = this.energy;
-        this.loadImages(this.endbossWalking);
-        this.loadImages(this.endbossAlert);
-        this.loadImages(this.endbossAttack);
-        this.loadImages(this.endbossHurt);
-        this.loadImages(this.endbossDead);
-        this.x = 2800;
-        this.animate();
+  constructor(world) {
+    super();
+    this.world = world;
+    this.maxEnergy = this.energy;
+    this.loadImages(this.endbossWalking);
+    this.loadImages(this.endbossAlert);
+    this.loadImages(this.endbossAttack);
+    this.loadImages(this.endbossHurt);
+    this.loadImages(this.endbossDead);
+    this.x = 2800;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => this.updateState(), 1000 / 60);
+    setInterval(() => this.updateAnimation(), 150);
+  }
+
+  animateWalking() {
+    this.playAnimation(this.endbossWalking);
+  }
+
+  animateAlert() {
+    this.playAnimation(this.endbossAlert);
+  }
+
+  animateAttack() {
+    this.playAnimation(this.endbossAttack);
+  }
+
+  animateHurt() {
+    this.playAnimation(this.endbossHurt);
+  }
+
+  animateDead() {
+    if (!this.isDeadAnimationPlaying) {
+      this.isDeadAnimationPlaying = true;
+      this.deadAnimationFrame = 0;
     }
-
-    animate() {
-        setInterval(() => this.updateState(), 1000 / 60);
-        setInterval(() => this.updateAnimation(), 150);
+    if (this.deadAnimationFrame < this.endbossDead.length) {
+      this.setImageFromCache(this.endbossDead, this.deadAnimationFrame++);
+    } else {
+      this.setImageFromCache(this.endbossDead, this.endbossDead.length - 1);
     }
+  }
 
-    animateWalking() {
-        this.playAnimation(this.endbossWalking);
+  updateAnimation() {
+    if (this.turning) return this.animateAlert();
+    this.resetAnimationIfStateChanged();
+    if (this.isDead()) return this.animateDead();
+    this.animateByState();
+  }
+
+  resetAnimationIfStateChanged() {
+    if (this.lastState !== this.state) {
+      this.currentImage = 0;
+      this.lastState = this.state;
     }
+  }
 
-    animateAlert() {
-        this.playAnimation(this.endbossAlert);
+  animateByState() {
+    const map = {
+      dead: () => this.animateDead(),
+      hurt: () => this.animateHurt(),
+      alert: () => this.animateAlert(),
+      chase: () => this.animateWalking(),
+      return: () => this.animateWalking(),
+      attack: () => this.animateAttack(),
+      idle: () => this.animateAlert(),
+    };
+    (map[this.state] || map["idle"])();
+  }
+
+  updateState() {
+    if (!this.canUpdateState()) return;
+    this.handleDetection();
+    this.transitionFromAlert();
+    this.handleCurrentState();
+  }
+
+  canUpdateState() {
+    return !this.isDead() && this.world && this.world.character;
+  }
+
+  transitionFromAlert() {
+    if (
+      this.state === "alert" &&
+      Date.now() - this.alertStartTime > 800 &&
+      !this.turning
+    ) {
+      this.state = "chase";
     }
+  }
 
-    animateAttack() {
-        this.playAnimation(this.endbossAttack);
+  handleCurrentState() {
+    const map = {
+      idle: () => this.checkIdleDistance(),
+      alert: () => this.handleAlert(),
+      chase: () => this.handleChase(),
+      attack: () => this.handleAttack(),
+      return: () => this.handleReturn(),
+    };
+    (map[this.state] || (() => {}))();
+  }
+
+  checkIdleDistance() {
+    if (Math.abs(this.world.character.x - this.x) < 400) {
+      this.state = "alert";
+      this.alertPlayed = false;
+      this.turning = true;
+      this.turnStartTime = Date.now();
     }
+  }
 
-    animateHurt() {
-        this.playAnimation(this.endbossHurt);
+  handleDetection() {
+    const character = this.world.character;
+    const distance = Math.abs(character.x - this.x);
+    if (distance < 400) {
+      this.lastTimeSeen = Date.now();
+      if (this.state === "idle") {
+        this.state = "alert";
+        this.alertPlayed = false;
+        this.turning = true;
+        this.turnStartTime = Date.now();
+      }
     }
+  }
 
-
-    animateDead() {
-        if (!this.isDeadAnimationPlaying) {
-            this.isDeadAnimationPlaying = true;
-            this.deadAnimationFrame = 0;
-        }
-
-        if (this.deadAnimationFrame < this.endbossDead.length) {
-            this.setImageFromCache(this.endbossDead, this.deadAnimationFrame++);
-        } else {
-            this.setImageFromCache(this.endbossDead, this.endbossDead.length - 1);
-        }
-    }
-
-    updateAnimation() {
-        if (this.lastState !== this.state) {
-            this.currentImage = 0;
-            this.lastState = this.state;
-        }
-
-        if (this.isDead()) {
-            this.animateDead();
-            return;
-        }
-
-        switch (this.state) {
-            case 'dead':
-                this.animateDead();
-                break;
-            case 'hurt':
-                this.animateHurt();
-                break;
-            case 'alert':
-                this.animateAlert();
-                break;
-            case 'chase':
-            case 'return':
-                this.animateWalking();
-                break;
-            case 'idle':
-            default:
-                this.animateAlert();
-                break;
-        }
-    }
-
-    updateState() {
-        if (this.state === 'alert') {
-            if (Date.now() - this.alertStartTime > 800) {
-                this.state = 'chase';
-            }
-        }
-        if (this.isDead()) return;
-        if (!this.world || !this.world.character) return;
-
-        this.handleDetection();
-
-        switch (this.state) {
-            case 'alert':
-                this.handleAlert();
-                break;
-            case 'chase':
-                this.handleChase();
-                break;
-            case 'attack':
-                this.handleAttack();
-                break;
-            case 'return':
-                this.handleReturn();
-                break;
-        }
-    }
-
-    handleDetection() {
+  handleAlert() {
+    if (this.alertPlayed) return;
+    this.alertPlayed = true;
+    this.alertStartTime = Date.now();
+    setTimeout(() => {
+      if (!this.isDead() && this.world.character) {
         const character = this.world.character;
-        const distance = Math.abs(character.x - this.x);
+        this.otherDirection = character.x < this.x ? false : true;
+        this.turning = false;
+        this.state = "chase";
+      }
+    }, this.turnDuration);
+  }
 
-        if (distance < 600) {
-            this.lastTimeSeen = Date.now();
+  handleChase() {
+    const character = this.world.character;
+    const distance = character.x - this.x;
+    if (this.turning) return;
+    this.otherDirection = distance < 0 ? false : true;
+    if (distance < -150) this.moveLeft();
+    else if (distance > 150) this.moveRight();
+    if (Math.abs(distance) <= 150) this.state = "attack";
+    if (Math.abs(distance) > 800 && Date.now() - this.lastTimeSeen > 3000)
+      this.state = "return";
+  }
 
-            if (this.state === 'idle') {
-                this.state = 'alert';
-                this.alertPlayed = false;
-            }
-        }
+  handleAttack() {
+    const character = this.world.character;
+    const distance = character.x - this.x;
+    this.otherDirection = distance < 0 ? false : true;
+    if (Math.abs(distance) > 150) {
+      this.state = "chase";
+      return;
     }
+    if (distance < -30) this.moveLeft();
+    else if (distance > 30) this.moveRight();
+    this.tryAttack();
+  }
 
-    handleAlert() {
-        if (this.alertPlayed) return;
+  handleReturn() {
+    if (this.isNearCharacter()) return this.startAlert();
+    this.moveTowardsSpawn();
+    this.finishReturnIfClose();
+  }
 
-        this.alertPlayed = true;
-        this.alertStartTime = Date.now();
+  isNearCharacter() {
+    return Math.abs(this.world.character.x - this.x) < 300;
+  }
+
+  startAlert() {
+    this.state = "alert";
+    this.alertPlayed = false;
+    this.turning = true;
+    this.turnStartTime = Date.now();
+  }
+
+  moveTowardsSpawn() {
+    if (this.x < this.spawnX) {
+      this.moveRight();
+      this.otherDirection = true;
+    } else if (this.x > this.spawnX) {
+      this.moveLeft();
+      this.otherDirection = false;
     }
+  }
 
-    handleChase() {
-        const character = this.world.character;
-        const distance = Math.abs(character.x - this.x);
-        const now = Date.now();
-
-        if (distance < 150) {
-            this.state = 'attack';
-            return;
-        }
-
-        this.moveTowardsCharacter();
-
-        if (distance > 800 && now - this.lastTimeSeen > 3000) {
-            this.state = 'return';
-        }
+  finishReturnIfClose() {
+    if (Math.abs(this.x - this.spawnX) < 5) {
+      this.x = this.spawnX;
+      this.state = "idle";
+      this.alertPlayed = false;
     }
+  }
 
-    handleAttack() {
-        const character = this.world.character;
-        const distance = Math.abs(character.x - this.x);
+  moveRight() {
+    this.x += this.speed;
+  }
 
-        if (distance > 200) {
-            this.state = 'chase';
-            return;
-        }
+  moveLeft() {
+    this.x -= this.speed;
+  }
 
-        this.tryAttack();
+  tryAttack() {
+    const now = Date.now();
+    if (now - this.lastAttack < this.attackCooldown) return;
+    const character = this.world.character;
+    if (this.isColliding(character)) {
+      character.hit(10);
+      this.world.statusbarHealth.setPercentage(character.energy);
+      this.lastAttack = now;
     }
+  }
 
-    handleReturn() {
-        this.returnToSpawn();
-
-        if (Math.abs(this.x - this.spawnX) < 5) {
-            this.x = this.spawnX;
-            this.state = 'idle';
-            this.alertPlayed = false;
-        }
+  hit(damage = 10) {
+    if (this.isDead()) return;
+    this.energy -= damage;
+    if (this.energy <= 0) {
+      this.energy = 0;
+      this.state = "dead";
+      return;
     }
-
-    moveTowardsCharacter() {
-        const character = this.world.character;
-
-        if (character.x < this.x) {
-            this.moveLeft();
-            this.otherDirection = false;
-        }
-    }
-
-    returnToSpawn() {
-        if (this.x < this.spawnX) {
-            this.moveRight();
-            this.otherDirection = true;
-        }
-    }
-
-
-    tryAttack() {
-        const now = Date.now();
-        if (now - this.lastAttack < this.attackCooldown) return;
-
-        const character = this.world.character;
-
-        if (this.isColliding(character)) {
-            character.hit(10);
-            this.world.statusbarHealth.setPercentage(character.energy);
-            this.lastAttack = now;
-        }
-    }
-
-    hit(damage = 10) {
-        if (this.isDead()) return;
-
-        this.energy -= damage;
-
-        if (this.energy <= 0) {
-            this.energy = 0;
-            this.state = 'dead';
-            return;
-        }
-        this.state = 'hurt';
-
-        clearTimeout(this.hurtTimeout);
-        this.hurtTimeout = setTimeout(() => {
-            if (this.energy > 0) {
-                this.state = 'chase';
-            }
-        }, 500);
-    }
+    this.state = "hurt";
+    clearTimeout(this.hurtTimeout);
+    this.hurtTimeout = setTimeout(() => {
+      if (this.energy > 0) {
+        this.state = "chase";
+      }
+    }, 500);
+  }
 }
