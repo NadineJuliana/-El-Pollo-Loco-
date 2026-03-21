d9a8ee733a1706a01f78f727c84b806e4318a784
NadineJuliana <nadine.juliana.b@web.de>
2026-03-21 12:49:04 +0100

    add open/close functions for  dialogs

---
+++ js/ui.js
@@ -0,0 +1,15 @@
+function openInstructions() {
+  document.getElementById("instructions").showModal();
+}
+
+function openImprint() {
+  document.getElementById("imprint").showModal();
+}
+
+function closeInstructions() {
+  document.getElementById("instructions").close();
+}
+
+function closeImprint() {
+  document.getElementById("imprint").close();
+}
