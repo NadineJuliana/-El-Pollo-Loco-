098caaf1e2c19071ed6d94b113f0ce3ceecab71f
NadineJuliana <nadine.juliana.b@web.de>
2026-03-23 15:57:50 +0100

    add updateUI and show loose/win screen on ui.js

--- js/ui.js
+++ js/ui.js
@@ -13,3 +13,38 @@ function closeInstructions() {
 function closeImprint() {
   document.getElementById("imprint").close();
 }
+
+function updateUI() {
+  if (!world) return;
+  const overlay = document.querySelector(".ui__overlay");
+  if (world.isGameOver) {
+    showLoseScreen();
+  }
+  if (world.isGameWon) {
+    showWinScreen();
+  }
+}
+
+function showLoseScreen() {
+  document.getElementById("loseScreen").classList.remove("d-none");
+}
+
+function showWinScreen() {
+  document.getElementById("winScreen").classList.remove("d-none");
+}
+
+function backHome() {}
+
+function restartGame() {}
+
+function toggleSound() {
+  if (AudioHub.muted) {
+    AudioHub.unmuteAll();
+  } else {
+    AudioHub.muteAll();
+  }
+}
+
+function toggleScreen() {}
+
+function toggleControls() {}
