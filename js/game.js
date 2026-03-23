feed9a08c8b6e399b0d99ac788007439cdf1f25b
NadineJuliana <nadine.juliana.b@web.de>
2026-03-23 15:58:41 +0100

    Enhance init function and add startGame function on game.js

--- js/game.js
+++ js/game.js
@@ -1,41 +1,38 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let audioStarted = false;
 
 function init() {
-    canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
+  canvas = document.getElementById("canvas");
+  IntervalHub.stopAllIntervals();
+  AudioHub.initSounds();
+  AudioHub.startBackgroundMusic();
+  world = new World(canvas, keyboard);
+}
 
-
-    console.log('My Character is', world.character);
-
+function startGame() {
+  document.getElementById("startscreen").style.display = "none";
+  document.getElementById("canvas").style.display = "block";
+  AudioHub.startBackgroundMusic();
+  init();
 }
 
 
-window.addEventListener('keydown', (e) => {
-    if (e.keyCode == 39)
-        keyboard.RIGHT = true;
-    if (e.keyCode == 37)
-        keyboard.LEFT = true;
-    if (e.keyCode == 38)
-        keyboard.UP = true;
-    if (e.keyCode == 32)
-        keyboard.SPACE = true;
-    if (e.keyCode == 68)
-        keyboard.D = true
-    // console.log(e);
-})
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode == 39) keyboard.RIGHT = true;
+  if (e.keyCode == 37) keyboard.LEFT = true;
+  if (e.keyCode == 38) keyboard.UP = true;
+  if (e.keyCode == 32) keyboard.SPACE = true;
+  if (e.keyCode == 68) keyboard.D = true;
+  // console.log(e);
+});
 
-window.addEventListener('keyup', (e) => {
-    if (e.keyCode == 39)
-        keyboard.RIGHT = false;
-    if (e.keyCode == 37)
-        keyboard.LEFT = false;
-    if (e.keyCode == 38)
-        keyboard.UP = false;
-    if (e.keyCode == 32)
-        keyboard.SPACE = false;
-    if (e.keyCode == 68)
-        keyboard.D = false;
-    // console.log(e);
-})
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode == 39) keyboard.RIGHT = false;
+  if (e.keyCode == 37) keyboard.LEFT = false;
+  if (e.keyCode == 38) keyboard.UP = false;
+  if (e.keyCode == 32) keyboard.SPACE = false;
+  if (e.keyCode == 68) keyboard.D = false;
+  // console.log(e);
+});
