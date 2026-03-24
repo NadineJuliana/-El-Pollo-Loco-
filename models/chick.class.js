005d348b778064af9e043b65e2de795cf03d330c
NadineJuliana <nadine.juliana.b@web.de>
2026-03-25 00:05:27 +0100

    add IntervalHub to enemys

--- models/chick.class.js
+++ models/chick.class.js
@@ -1,48 +1,46 @@
 class Chick extends MovableObject {
-    y = 380;
-    height = 30;
-    width = 40;
-    offset = { top: 5, right: 5, bottom: 5, left: 5 };
+  y = 380;
+  height = 30;
+  width = 40;
+  offset = { top: 5, right: 5, bottom: 5, left: 5 };
 
-    isDeadAnimationPlaying = false;
+  isDeadAnimationPlaying = false;
 
-    littleChickenWalking = ImageHub.chicken_small.walk;
-    littleChickenDead = ImageHub.chicken_small.dead;
+  littleChickenWalking = ImageHub.chicken_small.walk;
+  littleChickenDead = ImageHub.chicken_small.dead;
 
-    constructor() {
-        super().loadImage('img/2_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.loadImages(this.littleChickenWalking);
-        this.loadImages(this.littleChickenDead);
-        this.x = 800 + Math.random() * 800;
-        this.speed = 0.15 + Math.random() * 0.5;
-        this.animate();
-    }
+  constructor() {
+    super().loadImage("img/2_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.littleChickenWalking);
+    this.loadImages(this.littleChickenDead);
+    this.x = 800 + Math.random() * 800;
+    this.speed = 0.15 + Math.random() * 0.5;
+  }
 
+  die() {
+    this.isDeadAnimationPlaying = true;
+    this.setImageFromCache(this.littleChickenDead, 0);
+    setTimeout(() => {
+      if (this.world) {
+        const index = this.world.level.enemies.indexOf(this);
+        if (index > -1) {
+          this.world.level.enemies.splice(index, 1);
+        }
+      }
+    }, 1000);
+  }
 
-    die() {
-        this.isDeadAnimationPlaying = true;
+  animate() {
+    IntervalHub.startInterval(() => {
+      if (!this.isDeadAnimationPlaying) this.moveLeft();
+    }, 1000 / 60);
+
+    IntervalHub.startInterval(() => {
+      if (!this.isDeadAnimationPlaying) {
+        this.playAnimation(this.littleChickenWalking);
+      } else {
         this.setImageFromCache(this.littleChickenDead, 0);
-        setTimeout(() => {
-            if (this.world) {
-                const index = this.world.level.enemies.indexOf(this);
-                if (index > -1) {
-                    this.world.level.enemies.splice(index, 1);
-                }
-            }
-        }, 1000);
-    }
-
-    animate() {
-        this.movementInterval = setInterval(() => {
-            if (!this.isDeadAnimationPlaying) this.moveLeft();
-        }, 1000 / 60);
-
-        this.animationInterval = setInterval(() => {
-            if (!this.isDeadAnimationPlaying) {
-                this.playAnimation(this.littleChickenWalking);
-            } else {
-                this.setImageFromCache(this.littleChickenDead, 0);
-            }
-        }, 200);
-    }
+      }
+    }, 200);
+  }
 }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,49 +1,46 @@
 class Chicken extends MovableObject {
-    y = 365;
-    height = 50;
-    width = 70;
-    offset = { top: 5, right: 5, bottom: 10, left: 5 };
+  y = 365;
+  height = 50;
+  width = 70;
+  offset = { top: 5, right: 5, bottom: 10, left: 5 };
 
-    isDeadAnimationPlaying = false;
+  isDeadAnimationPlaying = false;
 
-    bigChickenWalking = ImageHub.chicken_normal.walk;
-    bigChickenDead = ImageHub.chicken_normal.dead;
+  bigChickenWalking = ImageHub.chicken_normal.walk;
+  bigChickenDead = ImageHub.chicken_normal.dead;
 
-    constructor() {
-        super().loadImage('img/2_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.loadImages(this.bigChickenWalking);
-        this.loadImages(this.bigChickenDead);
-        this.x = 600 + Math.random() * 1000;
-        this.speed = 0.15 + Math.random() * 0.25;
-        this.animate();
-    }
+  constructor() {
+    super().loadImage("img/2_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.bigChickenWalking);
+    this.loadImages(this.bigChickenDead);
+    this.x = 600 + Math.random() * 1000;
+    this.speed = 0.15 + Math.random() * 0.25;
+  }
 
-    die() {
-        this.isDeadAnimationPlaying = true;
+  die() {
+    this.isDeadAnimationPlaying = true;
+    this.setImageFromCache(this.bigChickenDead, 0);
+    setTimeout(() => {
+      if (this.world) {
+        const index = this.world.level.enemies.indexOf(this);
+        if (index > -1) {
+          this.world.level.enemies.splice(index, 1);
+        }
+      }
+    }, 1000);
+  }
+
+  animate() {
+    IntervalHub.startInterval(() => {
+      if (!this.isDeadAnimationPlaying) this.moveLeft();
+    }, 1000 / 60);
+
+    IntervalHub.startInterval(() => {
+      if (!this.isDeadAnimationPlaying) {
+        this.playAnimation(this.bigChickenWalking);
+      } else {
         this.setImageFromCache(this.bigChickenDead, 0);
-        setTimeout(() => {
-            if (this.world) {
-                const index = this.world.level.enemies.indexOf(this);
-                if (index > -1) {
-                    this.world.level.enemies.splice(index, 1);
-                }
-            }
-        }, 1000);
-    }
-
-
-    animate() {
-        this.movementInterval = setInterval(() => {
-            if (!this.isDeadAnimationPlaying) this.moveLeft();
-        }, 1000 / 60);
-
-        this.animationInterval = setInterval(() => {
-            if (!this.isDeadAnimationPlaying) {
-                this.playAnimation(this.bigChickenWalking);
-            } else {
-                this.setImageFromCache(this.bigChickenDead, 0);
-            }
-        }, 200);
-    }
-
+      }
+    }, 200);
+  }
 }
