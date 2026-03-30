556a0d76c14e3f1bbf2fb0e0a02f2776720d3f71
NadineJuliana <nadine.juliana.b@web.de>
2026-03-30 14:22:58 +0200

    enhance keyboard class with static setControl and setMobileControl functions

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,8 +1,65 @@
 class Keyboard {
-    RIGHT = false;
-    LEFT = false;
-    UP = false;
-    SPACE = false;
-    D = false;
+  static RIGHT = false;
+  static LEFT = false;
+  static SPACE = false;
+  static D = false;
 
+  static setControls() {
+    window.addEventListener("keydown", (e) => {
+      if (e.key == "ArrowRight") {
+        Keyboard.RIGHT = true;
+      }
+      if (e.key == "ArrowLeft") {
+        Keyboard.LEFT = true;
+      }
+      if (e.key == " ") {
+        Keyboard.SPACE = true;
+      }
+      if (e.key == "d") {
+        Keyboard.D = true;
+      }
+    });
+
+    window.addEventListener("keyup", (e) => {
+      if (e.key == "ArrowRight") {
+        Keyboard.RIGHT = false;
+      }
+      if (e.key == "ArrowLeft") {
+        Keyboard.LEFT = false;
+      }
+      if (e.key == " ") {
+        Keyboard.SPACE = false;
+      }
+      if (e.key == "d") {
+        Keyboard.D = false;
+      }
+    });
+  }
+
+  static setMobileControls() {
+    document.getElementById("btnRight").addEventListener("touchstart", () => {
+      Keyboard.RIGHT = true;
+    });
+    document.getElementById("btnRight").addEventListener("touchend", () => {
+      Keyboard.RIGHT = false;
+    });
+    document.getElementById("btnLeft").addEventListener("touchstart", () => {
+      Keyboard.LEFT = true;
+    });
+    document.getElementById("btnLeft").addEventListener("touchend", () => {
+      Keyboard.LEFT = false;
+    });
+    document.getElementById("btnJump").addEventListener("touchstart", () => {
+      Keyboard.SPACE = true;
+    });
+    document.getElementById("btnJump").addEventListener("touchend", () => {
+      Keyboard.SPACE = false;
+    });
+    document.getElementById("btnThrow").addEventListener("touchstart", () => {
+      Keyboard.D = true;
+    });
+    document.getElementById("btnThrow").addEventListener("touchend", () => {
+      Keyboard.D = false;
+    });
+  }
 }
